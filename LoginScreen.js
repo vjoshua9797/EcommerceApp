c476b2e3171e0e7eebfc94a24a7f599f7386a159
vjoshua9797 <114119575+vjoshua9797@users.noreply.github.com>
2023-08-27 21:11:18 +0800

    Update LoginScreen.js

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,3 +1,25 @@
-document.querySelector(".btn-primary").addEventListener("click", function () {
-  alert("login function not yet done");
+$(".btn-primary").click(async function () {
+  let username = $("input[placeholder='Username']").val();
+  let password = await fetch(
+    `https://personal-aswbdmxa.outsystemscloud.com/Ecommerce_API/rest/Account/HashPassword`,
+    {
+      method: "Get",
+      headers: {
+        Text: `${$("input[placeholder='Password']").val()}`,
+      },
+    }
+  );
+  password = await password.text(Response);
+  let loginApiResponse = await fetch(
+    `https://personal-aswbdmxa.outsystemscloud.com/Ecommerce_API/rest/Account/Login`,
+    {
+      method: "Get",
+      headers: {
+        Username: username,
+        Password: password,
+      },
+    }
+  );
+  loginApiResponse = await loginApiResponse.json();
+  alert(loginApiResponse.Message);
 });
